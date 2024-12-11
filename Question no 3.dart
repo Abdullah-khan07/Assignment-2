0831b9c4c85407eea9bf8fdf8924145bce2a5416
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2024-12-11 01:21:58 -0800

    Answer no 2

---
+++ Question no 3.dart
@@ -0,0 +1,21 @@
+void main() {
+  List Days = [
+    "Sunday ",
+    "Saturday",
+    "Friday",
+    "Thursday",
+    "Wednesday",
+    "Tuesday",
+    "Monday"
+  ];
+  Days.removeLast();
+  print(Days);
+}
+// Answer is [Sunday , Saturday, Friday, Thursday, Wednesday, Tuesday]
+
+
+
+
+
+
+
