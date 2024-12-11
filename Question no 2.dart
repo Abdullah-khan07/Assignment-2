476bf815cf997f3f4cbe6489416a5d4011b739e7
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2024-12-11 01:14:17 -0800

    Answer no 2

---
+++ Question no 2.dart
@@ -0,0 +1,6 @@
+void main() {
+  List Days = [];
+  Days.add("Sunday ", "Saturday", "Friday", "Thursday", "Wednesday", "Tuesday","Monday");
+  print(Days);
+}
+// Answer is   Days.add("Sunday ", "Saturday", "Friday", "Thursday", "Wednesday", "Tuesday","Monday");
